29002e137f781f598ca3f68233f29753010f2874
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 09:17:06 +0530

    add RMS master

---
+++ controllers/bookingcontroller.js
@@ -0,0 +1,13 @@
+const Booking = require('../models/booking');
+
+// Create a new booking
+const createBooking = async (req, res) => {
+    // booking creation logic
+};
+
+// Get all bookings for a user
+const getUserBookings = async (req, res) => {
+    // logic to get user bookings
+};
+
+module.exports = { createBooking, getUserBookings };
