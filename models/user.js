1fd8627ba6dd0bfaa3e13c794bad04148fe88496
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 10:58:16 +0530

    add RMS master

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String, 
+        required: true },
+    email: { type: String, 
+        required: true, unique: true },
+    password: { type: String,
+         required: true },
     role: { type: String, enum: ['admin', 'user'], default: 'user' },
 }, { timestamps: true });
 
