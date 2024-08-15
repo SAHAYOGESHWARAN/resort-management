63999f5734e14ac80c4013006523254d6e5efe4a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 09:40:47 +0530

    add RMS master

---
+++ models/activity.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ActivitySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    availableSlots: { type: Number, required: true },
+    resort: { type: mongoose.Schema.Types.ObjectId, ref: 'Resort', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Activity', ActivitySchema);
