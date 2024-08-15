387b0f1f563a6d0997a5035cdb00a61e594a05f8
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 10:36:30 +0530

    add RMS master

--- server.js
+++ server.js
@@ -18,3 +18,5 @@ app.use('/api/activities', activityRoutes); // New Route
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const cors = require('cors');
+app.use(cors());
