ce64d267809b2666dd4819d5515062347480372c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 09:16:31 +0530

    add RMS master

---
+++ controllers/resortcontroller.js
@@ -0,0 +1,18 @@
+const Resort = require('../models/resort');
+
+// Get all resorts
+const getAllResorts = async (req, res) => {
+    // logic to fetch resorts
+};
+
+// Get a single resort by ID
+const getResortById = async (req, res) => {
+    // logic to fetch resort by ID
+};
+
+// Add a new resort (admin only)
+const addResort = async (req, res) => {
+    // logic to add new resort
+};
+
+module.exports = { getAllResorts, getResortById, addResort };
