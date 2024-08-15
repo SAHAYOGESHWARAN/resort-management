6f40634f1fb0f59eb526603d92b1461c9e68c89c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 09:15:47 +0530

    add RMS master

---
+++ controllers/usercontroller.js
@@ -0,0 +1,18 @@
+const User = require('../models/user');
+
+// Register new user
+const registerUser = async (req, res) => {
+    // registration logic
+};
+
+// Login user
+const loginUser = async (req, res) => {
+    // login logic
+};
+
+// Get user profile
+const getUserProfile = async (req, res) => {
+    // profile fetching logic
+};
+
+module.exports = { registerUser, loginUser, getUserProfile };
