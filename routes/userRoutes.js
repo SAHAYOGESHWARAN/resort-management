c02de17e643b1de42080d12b75e7f8e445b87b4d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 11:01:18 +0530

    add RMS master

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { registerUser, loginUser, getUserProfile } = require('../controllers/usercontroller');
 const router = express.Router();
 
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', getUserProfile);
