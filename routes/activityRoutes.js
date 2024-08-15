e25803ae8e8f6fb475c458334c4c6441adfae3ad
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 09:42:08 +0530

    add RMS master

---
+++ routes/activityRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { getAllActivities, getActivityById, addActivity, bookActivity } = require('../controllers/activityController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+// Get all activities for a resort
+router.get('/:resortId', getAllActivities);
+
+// Get a single activity by ID
+router.get('/activity/:id', getActivityById);
+
+// Add a new activity (admin only)
+router.post('/', protect, addActivity);
+
+// Book an activity
+router.post('/book/:id', protect, bookActivity);
+
+module.exports = router;
