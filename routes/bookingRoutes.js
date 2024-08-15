ca58cb2ee0c4aa389284843c083ad661f9b8c7f1
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 11:00:13 +0530

    add RMS master

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { createBooking, getUserBookings } = require('../controllers/bookingcontroller');
 const router = express.Router();
 
+
 router.post('/', createBooking);
 router.get('/user', getUserBookings);
+
 
 module.exports = router;
