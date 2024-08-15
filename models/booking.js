1a05cf31647ef7a2c85a87e624e3430029d235a6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 10:56:33 +0530

    add RMS master

--- models/booking.js
+++ models/booking.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    resort: { type: mongoose.Schema.Types.ObjectId, ref: 'Resort', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', required: true },
+    resort: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Resort', required: true },
     checkInDate: { type: Date, required: true },
     checkOutDate: { type: Date, required: true },
     totalPrice: { type: Number, required: true },
