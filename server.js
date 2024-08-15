6ac4407c16e7aa2612c1d328aece1557812b3896
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-15 11:08:33 +0530

    add RMS master

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const resortRoutes = require('./routes/resortRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
-const activityRoutes = require('./routes/activityRoutes'); // New Route
+const activityRoutes = require('./routes/activityRoutes'); 
 require('dotenv').config();
 
 connectDB();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/resorts', resortRoutes);
 app.use('/api/bookings', bookingRoutes);
-app.use('/api/activities', activityRoutes); // New Route
+app.use('/api/activities', activityRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
